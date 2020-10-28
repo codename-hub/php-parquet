60f75701969f0b0d252a4668061ef5564479580c
Kevin <kevin@jocoon.de>
2020-10-28 10:10:37 +0100

    exclude PHP <= 7.2 from tests, as it will fail at the moment

--- test_phpversions.ps1
+++ test_phpversions.ps1
@@ -1,5 +1,6 @@
 # this is a Powershell Script to test PHP 7.1-7.4 compatibility
-docker run --rm $(docker build --build-arg PHP_VERSION=7.1 -q -f Dockerfile.template .)
-docker run --rm $(docker build --build-arg PHP_VERSION=7.2 -q -f Dockerfile.template .)
+
+# docker run --rm $(docker build --build-arg PHP_VERSION=7.1 -q -f Dockerfile.template .)
+# docker run --rm $(docker build --build-arg PHP_VERSION=7.2 -q -f Dockerfile.template .)
 docker run --rm $(docker build --build-arg PHP_VERSION=7.3 -q -f Dockerfile.template .)
 docker run --rm $(docker build --build-arg PHP_VERSION=7.4 -q -f Dockerfile.template .)
--- test_phpversions.sh
+++ test_phpversions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # this is a Powershell Script to test PHP 7.1-7.4 compatibility
-docker run --rm $(docker build --build-arg PHP_VERSION=7.1 -q -f Dockerfile.template .)
-docker run --rm $(docker build --build-arg PHP_VERSION=7.2 -q -f Dockerfile.template .)
+
+# docker run --rm $(docker build --build-arg PHP_VERSION=7.1 -q -f Dockerfile.template .)
+# docker run --rm $(docker build --build-arg PHP_VERSION=7.2 -q -f Dockerfile.template .)
 docker run --rm $(docker build --build-arg PHP_VERSION=7.3 -q -f Dockerfile.template .)
 docker run --rm $(docker build --build-arg PHP_VERSION=7.4 -q -f Dockerfile.template .)
