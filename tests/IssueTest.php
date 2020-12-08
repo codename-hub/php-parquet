e253a6656f60482eaa298f7a64a4bebd8ee1a8a9
Kevin <kevin@jocoon.de>
2020-12-08 13:57:04 +0100

    test for parquet-dotnet's issue 87, marked incomplete, information/verification missing

--- tests/IssueTest.php
+++ tests/IssueTest.php
@@ -23,4 +23,36 @@ class IssueTest extends TestBase
       $this->assertEquals($i+1, $v);
     }
   }
+
+  /**
+   * @see https://github.com/aloneguid/parquet-dotnet/issues/87
+   */
+  public function testParquetDotnetIssue87(): void
+  {
+    $this->markTestIncomplete('Marked test as incomplete, need more information whether this really is an issue or not.');
+
+    $reader = new ParquetReader($this->openTestFile('issues/parquet-dotnet_87_running_numbers_spark.gz.parquet'));
+
+    $data = $reader->ReadEntireRowGroup();
+    $expectedRows = 10000;
+
+    $numbers = $data[0]->getData();
+    $txt = $data[1]->getData();
+
+    $this->assertCount($expectedRows, $numbers);
+    $this->assertCount($expectedRows, $txt);
+
+    for ($i=0; $i < $expectedRows; $i++) {
+      $num = $numbers[$i];
+      if($num % 100 === 0) {
+        $this->assertNull($txt[$i]);
+      } else {
+        $txtIndex = $num % 10;
+        $expected = implode('', array_fill(0, 1000, $txtIndex . ', '));
+        $actual = $txt[$i];
+
+        $this->assertEquals($expected, $actual, "Text doesn't match at Row {$i}");
+      }
+    }
+  }
 }
