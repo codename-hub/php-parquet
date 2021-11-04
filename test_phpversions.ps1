f1035a904b1d2201bd90e42c3c0ba7905ac27298
Kevin <kevin.dargel@onlinehomede>
2021-11-04 13:03:45 +0100

    some minor changes in Dockerfiles for local testing purposes

--- test_phpversions.ps1
+++ test_phpversions.ps1
@@ -2,5 +2,5 @@
 
 # docker run --rm $(docker build --build-arg PHP_VERSION=7.1 -q -f Dockerfile.template .)
 # docker run --rm $(docker build --build-arg PHP_VERSION=7.2 -q -f Dockerfile.template .)
-docker run --rm $(docker build --build-arg PHP_VERSION=7.3 -q -f Dockerfile.template .)
+docker run --rm $(docker build --build-arg PHP_VERSION=7.3.32 -q -f Dockerfile.template .)
 docker run --rm $(docker build --build-arg PHP_VERSION=7.4 -q -f Dockerfile.template .)
